c59a7be3c8126b598df8af1558230f3f9be54bcd
Veikko Arvonen <arvonenveikko@gmail.com>
2025-11-04 21:50:15 +0200

    Initial Commit

---
+++ Budjettisovellus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Budjettisovellus
+//
+//  Created by Veikko Arvonen on 4.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
